aee34152320d3a383af439fef782b8b7a784881d
pfraisse <pierrefraisse90@gmail.com>
2025-06-30 13:57:15 +0200

    feat(var): Added variables and types

---
+++ 01_variables.py
@@ -0,0 +1,40 @@
+### Variable declaration ###
+
+# Snake case
+first_var = 10
+# Should be 10
+print(first_var)
+
+second_var = 5
+# Should be 15
+print(first_var + second_var)
+
+
+### Types ###
+
+# int
+int_var = 5
+
+# float
+float_var = 3.4
+
+# string
+str_var = "Hi"
+
+# bool
+bool_var = True
+
+# empty
+empty_var = None
+
+
+### Comments  ###
+
+# This is a comment
+
+# This
+# is
+# a
+# multi
+# line
+# comment
